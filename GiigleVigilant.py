a3dc3735cf057196af66b52347c80cd2cc328126
OrangeMarine11 <180303324+OrangeMarine11@users.noreply.github.com>
2024-10-31 16:14:25 +0100

    4th test
    4th test of the code

--- GiigleVigilant.py
+++ GiigleVigilant.py
@@ -33,6 +33,20 @@ def move_right():
         x = 290
     player.setx()
 
+def fire_bullet():
+    global bullet_state
+    if bullet_state== "ready":
+        bullet_state = "fire"
+        x = player.xcor
+        y = player.ycor
+        bullet.setposition(x,y)
+        bullet.swowturtle()
 
+#Controls
+
+win.listen()
+win.onkey(move_left, "Left")
+win.onkey(move_right, "Right")
+win.onkey(fire_bullet, "space")
 while True:
     win.update()
