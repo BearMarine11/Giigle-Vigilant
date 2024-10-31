4b5c97b00276b2a67f182e5fb7427df361f4a947
OrangeMarine11 <180303324+OrangeMarine11@users.noreply.github.com>
2024-10-31 15:48:17 +0100

    Third test
    Third test of the code

--- GiigleVigilant.py
+++ GiigleVigilant.py
@@ -16,6 +16,22 @@ player.penup()
 player.speed(0)
 player.goto(0, -250)
 player.setheading(90)
+player_speed = 30
+bullet_speed = 3
+
+def move_left():
+    x = player.xcor()
+    x += player_speed
+    if x < -290:
+        x = -290
+player.setx(x)
+
+def move_right():
+    x = player.xcor()
+    x += player_speed
+    if x > 290:
+        x = 290
+    player.setx(x)
 
 while True:
     win.update()
