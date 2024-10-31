d5f73d39f85fc6318dacd131c4cecc74faf83745
OrangeMarine11 <180303324+OrangeMarine11@users.noreply.github.com>
2024-10-31 15:13:57 +0100

    Second test
    Second test of the code

--- GiigleVigilant.py
+++ GiigleVigilant.py
@@ -6,3 +6,16 @@ win.title("Giigle Vigilant")
 win.bgcolor("black")
 win.setup(width=600, height=800)
 win.tracer(0)
+
+#Player 
+
+player = turtle.Turtle()
+player.shape("Bear")
+player.color("orange")
+player.penup()
+player.speed(0)
+player.goto(0, -250)
+player.setheading(90)
+
+while True:
+    win.update()
