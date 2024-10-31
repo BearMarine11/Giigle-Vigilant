8032b6f4a1a8921c94420371c86b83b3ccda3059
OrangeMarine11 <180303324+OrangeMarine11@users.noreply.github.com>
2024-10-31 17:03:39 +0100

    6th test
    6th test of the code

--- GiigleVigilant.py
+++ GiigleVigilant.py
@@ -1,7 +1,7 @@
 import turtle
 import random
 
-win =turtle.Screen()
+win = turtle.Screen()
 win.title("Giigle Vigilant")
 win.bgcolor("black")
 win.setup(width=600, height=800)
@@ -17,7 +17,7 @@ player.speed(0)
 player.goto(0, -250)
 player.setheading(90)
 player_speed = 30
-bullet_speed = 3
+bullet_speed = 1
 
 def move_left():
     x = player.xcor()
@@ -37,10 +37,10 @@ def fire_bullet():
     global bullet_state
     if bullet_state== "ready":
         bullet_state = "fire"
-        x = player.xcor
-        y = player.ycor
+        x = player.xcor()
+        y = player.ycor() + 10
         bullet.setposition(x,y)
-        bullet.swowturtle()
+        bullet.showturtle()
 
 #Controls
 
@@ -48,5 +48,34 @@ win.listen()
 win.onkey(move_left, "Left")
 win.onkey(move_right, "Right")
 win.onkey(fire_bullet, "space")
+
+#Player arms
+
+bullet = turtle.Turtle()
+bullet.shape("circle")
+bullet.color("yellow")
+bullet.penup()
+bullet.speed(10)
+bullet.shapesize(stretch_wid=0.5, stretch_len=0.5)
+bullet.hideturtle()
+bullet_state ="ready"
+
+#Invanders
+
+num_invaders = 5
+invaders = []
+for _ in range(num_invaders):
+    invader = turtle.Turtle()
+    invader.shape("square")
+    invader.color(red)
+    invader.penup()
+    invader.speed(0)
+    x = random.randint(-290, 290)
+    y = random.randint(100, 250)
+    invaders.append(invader)
+    
 while True:
+    if bullet_state == "fire":
+        y = bullet.ycor()
+        y += bullet_speed
     win.update()
